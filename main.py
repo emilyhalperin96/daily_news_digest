dd4f97097c321324bec05336b4a9498670187a91
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP-2.lan>
2023-07-20 10:33:57 -0400

    limit news articles, made topic dynamic

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests 
 from send_email import send_email 
 
+topic = 'tesla'
+
 API_KEY = 'db202092afe140189ae03c36492fdd52'
 
-url = 'https://newsapi.org/v2/everything?q=tesla&from=2023-06-20&sortBy=publishedAt&apiKey=db202092afe140189ae03c36492fdd52' 
+url = f'https://newsapi.org/v2/everything?q={topic}&from=2023-06-20&sortBy=publishedAt&apiKey=db202092afe140189ae03c36492fdd52&language=en' 
 
 #Make a request 
 request = requests.get(url)
@@ -13,11 +15,10 @@ content = request.json()
 #print(content['articles'])
 
 #Access titles 
-
 body = ''
 
-for article in content['articles']:
-    body = body + article['title'] + '\n' + article['description'] + 2*'\n'
+for article in content['articles'][:20]:
+    body = "Subject: Today's news" + '\n' + body + article['title'] + '\n' + article['description'] + '\n' + article['url'] + 2*'\n'
 
 body = body.encode('utf-8')
 
