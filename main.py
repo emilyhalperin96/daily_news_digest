253e3aafab8df580ce2fd0afe2e0ea15e70f4e6d
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP-2.lan>
2023-07-20 10:20:13 -0400

    sent email with article title and description

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests 
+from send_email import send_email 
 
 API_KEY = 'db202092afe140189ae03c36492fdd52'
 
@@ -12,5 +13,12 @@ content = request.json()
 #print(content['articles'])
 
 #Access titles 
+
+body = ''
+
 for article in content['articles']:
-    print(article['title'])
+    body = body + article['title'] + '\n' + article['description'] + 2*'\n'
+
+body = body.encode('utf-8')
+
+send_email(body)
